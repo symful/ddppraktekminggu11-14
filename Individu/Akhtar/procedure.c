04e65a2c0e5b0848554761ab1d11fe91a6e98f5e
ExuNova <exufrostnova@gmail.com>
2025-11-13 13:12:13 +0700

    Up Tugas Individu

---
+++ Individu/Akhtar/procedure.c
@@ -0,0 +1,13 @@
+void kesimpulanKondisiKeuangan(int persentaseKondisiKeuangan, char kondisiKeuangan[8]){
+    if(persentaseKondisiKeuangan > 25 && strcmp(kondisiKeuangan, "Surplus") == 0){
+        printf("Anda termasuk mahasiswa hemat dan produktif.");
+    } else if (persentaseKondisiKeuangan > 0 && strcmp(kondisiKeuangan, "Surplus") == 0 ){
+        printf("Keuangan Anda seimbang, tetap waspada terhadap pengeluaran tidak perlu.");
+    } else if(persentaseKondisiKeuangan < 10 && strcmp(kondisiKeuangan, "Defisit") == 0 ){
+        printf("Anda mulai boros, perhatikan pengeluaran harian.");
+    } else if (strcmp(kondisiKeuangan, "Defisit") == 0){
+        printf("Kondisi keuangan kurang sehat. Kurangi pengeluaran dan cari tambahan pemasukan.");
+    } else {
+        printf("Keuangan Anda Seimbang");
+    }
+}
